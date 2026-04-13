fb38da6e3685e41cf3075570b42ee6462f5190d9
ghoshsanjit667-web <ghoshsanjit667@gmail.com>
2026-04-13 13:00:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-import threading
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import os
-
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Bot is running')
-
-def run_web():
-    port = int(os.environ.get("PORT", 10000))
-    server = HTTPServer(('0.0.0.0', port), Handler)
-    server.serve_forever()
-
-# Run web server in separate thread
-threading.Thread(target=run_web, daemon=True).start()
 from datetime import datetime
 import time
 import asyncio
